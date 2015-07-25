efcabf3a1d0f156f8ae52bac3c9bec980c5dd198
Andrew Pierce <apierce@Ashleys-MacBook-Pro-2.local>
2015-07-25 15:35:19 -0400

    first commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :votes, dependent: :destroy
+  validates :title, presence: true, uniqueness: true
+  validates :description, length: { maximum: 200 }, presence: true
+
+  def totals
+    votes.count
+  end
 end
